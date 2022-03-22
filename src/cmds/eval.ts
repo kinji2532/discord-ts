15b021c6bb1061b477a2398f1d0e2e7bc37279cc
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2022-03-23 08:45:40 +0900

    eval

--- src/cmds/eval.ts
+++ src/cmds/eval.ts
@@ -2,8 +2,7 @@ import { TextChannel } from 'discord.js';
 import { inspect } from 'util';
 import * as ts from "typescript";
 
-exports.func = (channel:TextChannel, code: string) =>{
-  const result = ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
-  console.log(result);
+exports.func = (channel:TextChannel, type: string, code: string) =>{
+  const result = (type === 'js' ? code:type === 'ts' ? ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }}):'');
   channel.send(inspect(eval(result.outputText)));
 }
