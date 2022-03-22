43a139d453d9012a20319519b654a34aafef1317
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2022-03-23 02:33:42 +0900

    eval

--- src/cmds/eval.ts
+++ src/cmds/eval.ts
@@ -3,7 +3,7 @@ import { inspect } from 'util';
 import * as ts from "typescript";
 
 export function(channel:TextChannel, code: tring) {
-  const result: Object = ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+  const result = ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
   console.log(result);
   channel.send(inspect(eval(result.outputText)));
 }
