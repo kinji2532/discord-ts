f6c2debbc508b59e286f90e50b7b05926654afac
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2022-03-23 08:47:06 +0900

    eval

--- src/cmds/eval.ts
+++ src/cmds/eval.ts
@@ -3,6 +3,6 @@ import { inspect } from 'util';
 import * as ts from "typescript";
 
 exports.func = (channel:TextChannel, type: string, code: string) =>{
-  const result = (type === 'js' ? code:type === 'ts' ? ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }}):'');
-  channel.send(inspect(eval(result.outputText)));
+  const result = (type === 'js' ? code:type === 'ts' ? ts.transpileModule(code, { compilerOptions: { module: ts.ModuleKind.CommonJS }}).outputText:'');
+  channel.send(inspect(eval(result)));
 }
